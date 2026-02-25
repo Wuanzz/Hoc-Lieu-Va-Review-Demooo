08273895347979e5b4b71453bf08e1efa53b9ad2
Minh Quan <quanchm2208@gmail.com>
2026-02-25 12:11:09 +0700

    Add project files.

---
+++ Hoc-Lieu-Va-Review-Demooo/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hoc_Lieu_Va_Review_Demooo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // public DbSet<Sach> Saches { get; set; }
+    }
+}
---
+++ Hoc-Lieu-Va-Review-Demooo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hoc_Lieu_Va_Review_Demooo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
