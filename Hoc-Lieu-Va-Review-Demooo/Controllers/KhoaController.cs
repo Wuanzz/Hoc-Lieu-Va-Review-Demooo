0bb4d2f0d03f44808c3473016504a436437a2813
Minh Quan <quanchm2208@gmail.com>
2026-02-25 17:23:19 +0700

    Create department

--- Hoc-Lieu-Va-Review-Demooo/Controllers/KhoaController.cs
+++ Hoc-Lieu-Va-Review-Demooo/Controllers/KhoaController.cs
@@ -23,5 +23,32 @@ namespace Hoc_Lieu_Va_Review_Demooo.Controllers
             var danhSachKhoa = await _context.Khoas.ToListAsync();
             return View(danhSachKhoa);
         }
+
+        // Action [GET] để hiển thị Form nhập liệu
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Action [POST] để nhận dữ liệu từ Form và lưu vào Database
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Chống tấn công giả mạo request
+        public async Task<IActionResult> Create([Bind("TenKhoa,MoTa")] Khoa khoa)
+        {
+            if (ModelState.IsValid)
+            {
+                // Thêm dữ liệu vào bộ nhớ đệm
+                _context.Add(khoa);
+                // Lưu chính thức xuống SQL Server
+                await _context.SaveChangesAsync();
+
+                // Lưu xong thì quay về trang danh sách (Index)
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Nếu dữ liệu không hợp lệ (ví dụ bỏ trống tên Khoa), thì hiển thị lại Form kèm báo lỗi
+            return View(khoa);
+        }
     }
 }
