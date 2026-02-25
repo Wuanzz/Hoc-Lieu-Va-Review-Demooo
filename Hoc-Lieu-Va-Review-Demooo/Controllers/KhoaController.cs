bfd4a9e508eae97c35e910e66783162dbc29ba6a
Minh Quan <quanchm2208@gmail.com>
2026-02-25 17:12:01 +0700

    Add department management menu

---
+++ Hoc-Lieu-Va-Review-Demooo/Controllers/KhoaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Hoc_Lieu_Va_Review_Demooo.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Hoc_Lieu_Va_Review_Demooo.Controllers
+{
+    // Yêu cầu phải đăng nhập mới được vào trang này
+    [Authorize]
+    public class KhoaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KhoaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action hiển thị danh sách Khoa
+        public async Task<IActionResult> Index()
+        {
+            // Lấy toàn bộ danh sách Khoa từ Database
+            var danhSachKhoa = await _context.Khoas.ToListAsync();
+            return View(danhSachKhoa);
+        }
+    }
+}
